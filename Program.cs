2f410eb1d60fa279cc05930c037612047c97f5ab
Jonathan Rupp <jrupp@rightpoint.com>
2017-06-22 12:50:50 -0500

    Ignore hidden worksheets and try harder to build good field names

--- Program.cs
+++ Program.cs
@@ -36,6 +36,9 @@ namespace ToSql
                         {
                             foreach (var s in wkb.Worksheets)
                             {
+                                // ignore hidden worksheets
+                                if (s.Visibility != XLWorksheetVisibility.Visible)
+                                    continue;
                                 Process(outTw, file, s.Name, new CsvReader(new ExcelParser(s)));
                             }
                         }
@@ -49,7 +52,7 @@ namespace ToSql
             var tableName = (Path.GetFileNameWithoutExtension(file) + "_" + sheet).TrimEnd('_');
             reader.ReadHeader();
             tw.WriteLine($"declare @{tableName} table (");
-            tw.WriteLine(string.Join("," + Environment.NewLine, reader.FieldHeaders.Select(h => $"  [{h}] nvarchar(max)")));
+            tw.WriteLine(string.Join("," + Environment.NewLine, reader.FieldHeaders.Select(h => $"  [{h.Trim().Replace("?", "")}] nvarchar(max)")));
             tw.WriteLine(");");
 
             tw.WriteLine("BEGIN");
